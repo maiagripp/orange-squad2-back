b47b651cca19044748ccc4e73016de64c7ea6659
Italo <italosp00@gmail.com>
2021-09-03 21:46:20 -0300

    Add middlewares and routes

--- src/server.js
+++ src/server.js
@@ -1,8 +1,23 @@
-const server = require("express");
-const app = server();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const routes = require("./routes/routes");
 
-const PORT = 3000;
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 
+// middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(routes);
+app.use(cors());
+app.use(helmet());
+
+// server
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}/`);
 });
