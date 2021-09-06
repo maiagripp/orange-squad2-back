c96ac254fa967eced5846c385db5b58edb25ebca
Italo <italosp00@gmail.com>
2021-09-05 22:55:19 -0300

    Add the register controller to the route

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,12 +1,12 @@
+const { register } = require("../controllers/controllers");
+
 const router = require("express").Router();
 
 router
   .post("/login", (req, res) => {
     return res.status(200).json({ status: 200, msg: "success" });
   })
-  .post("/signup", (req, res) => {
-    return res.status(201).json({ status: 201, msg: "success" });
-  })
+  .post("/register", register)
   .post("/workstation", (req, res) => {
     return res.status(201).json({ status: 201, msg: "success" });
   })
