b5a9a84f85725f0c8823c34c9e6edc32b25142a0
Italo <italosp00@gmail.com>
2021-09-06 17:03:37 -0300

    Create uri to connect with mongo atlas

--- src/database/index.js
+++ src/database/index.js
@@ -1,6 +1,13 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
+// prettier-ignore
+const uri = process.env.NODE_ENV === "development" ? process.env.MONGO_TEST : process.env.MONGO_PROD
+console.log(uri);
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-mongoose.connect("mongodb://localhost/test");
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
