92ed7711c93a110e736bf7e2a2e8789298dc5fbb
Italo <italosp00@gmail.com>
2021-09-07 14:23:21 -0300

    Create controllers for login and scheduling

--- src/database/index.js
+++ src/database/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 // prettier-ignore
 const uri = process.env.NODE_ENV === "development" ? process.env.MONGO_TEST : process.env.MONGO_PROD
-console.log(uri);
+
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
