1e6f14ff0a6b5e97c3a2fabf4e58a78560b1fdfc
Italo <italosp00@gmail.com>
2021-09-09 15:07:48 -0300

    Create new model

---
+++ src/database/models/chair.js
@@ -0,0 +1,22 @@
+const mongoose = require("../index");
+
+const ChairSchema = new mongoose.Schema({
+  chair: {
+    type: Number,
+    unique: true,
+    required: true,
+  },
+  assignedTo: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Scheduling",
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Chair = mongoose.model("Chair", ChairSchema);
+
+module.exports = Chair;
