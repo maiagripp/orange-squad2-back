949a8b5997c214e2aa8812b39dc26e49a7e351d7
Italo <italosp00@gmail.com>
2021-09-09 20:59:47 -0300

    Create new routes, to send the email and to reset the password

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,17 +1,21 @@
 const {
   register,
-  login,
+  authenticate,
   scheduling,
   users,
+  passRecovery,
+  passReset,
 } = require("../controllers/controllers");
 const authMiddleware = require("../middlewares/check-auth");
 
 const router = require("express").Router();
 
 router
-  .post("/login", login)
+  .post("/authenticate", authenticate)
   .post("/register", register)
   .post("/scheduling", authMiddleware, scheduling)
+  .post("/forgot_password", passRecovery)
+  .post("/reset_password", passReset)
   .get("/resume", (req, res) => {
     return res.status(200).json({ status: 201, msg: "success" });
   })
