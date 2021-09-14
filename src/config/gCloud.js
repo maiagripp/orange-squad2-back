44e96fd06ad95646e0e7ca946dc44570675a6f04
Italo <italosp00@gmail.com>
2021-09-14 16:23:30 -0300

    Add google cloud lib to handle the users images

---
+++ src/config/gCloud.js
@@ -0,0 +1,12 @@
+const Cloud = require("@google-cloud/storage");
+const path = require("path");
+
+const serviceKey = path.join(__dirname, "keys.json");
+
+const { Storage } = Cloud;
+const storage = new Storage({
+  keyFilename: serviceKey,
+  projectId: "orange_fc",
+});
+
+module.exports = storage;
