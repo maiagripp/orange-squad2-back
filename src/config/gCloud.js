e5ea83e7b3bd30467140bc021d68ae5d58caaf29
Italo <italosp00@gmail.com>
2021-09-15 22:48:21 -0300

    Fix the path to the google cloud api keys

--- src/config/gCloud.js
+++ src/config/gCloud.js
@@ -1,7 +1,7 @@
 const Cloud = require("@google-cloud/storage");
 const path = require("path");
 
-const serviceKey = path.join(__dirname, "keys.json");
+const serviceKey = path.join(__dirname, "googleCloud/keys.json");
 
 const { Storage } = Cloud;
 const storage = new Storage({
